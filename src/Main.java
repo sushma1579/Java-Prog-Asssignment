989d9f4c0942d985ca8c112f96c25092c9fa52e2
sushma1579 <107958996+sushma1579@users.noreply.github.com>
2023-04-08 19:30:40 -0500

    Update Main.java
    added comments and complexities

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ import java.util.Map;
 
 public class Main {
     static HashMap<Integer,Order> orderlist = new HashMap<>();
-    static int key=1;
+    static int key=1; // variable to store the respective keys
 
     public static void main(String[] args) {
         Order order1 = new Order("lamb");
@@ -21,7 +21,8 @@ public class Main {
         Order order3 = new Order("fish");
         Order order4 = new Order("lamb");
         Order order5 = new Order("chicken");
-
+        
+        nextOrder();
         takeOrder(order1);
         takeOrder(order2);
         takeOrder(order3);
@@ -30,58 +31,37 @@ public class Main {
         takeOrder(order5);
         nextOrder();
     }
-
+    
+    //Overall complexity of this method O(n^2) , assuming number of orders <=10~20 always
     public static Order nextOrder() {
+        // if orderlist is empty
+        // complexity O(1)
         if (orderlist.isEmpty()) {
             System.out.println("Order List is empty!! Let's wait for orders");
             return null;
         }
 
         // if list size = 1
+        // complexity O(1)
         if (orderlist.size() == 1) {
             Order order = orderlist.values().iterator().next();
             System.out.println(order.getFoodType() + " Order Ready!");
-            orderlist.remove(1);
+            orderlist.remove(1); // delete the items from orderlist once delivered/prepared
             return order;
         }
 
         //  if size>1 , look for fish first then chicken and then lamb
+        // complexity O(n^2) but size the orderlist is never going to be too big , so this can still be low
         for (String foodType : new String[]{"fish", "chicken", "lamb"}) {
-            orderlist.values().removeIf(order -> order.getFoodType().equals(foodType));
+            orderlist.values().removeIf(order -> order.getFoodType().equals(foodType)); // delete the items from orderlist once delivered/prepared
             System.out.println(foodType + " Order Ready!");
         }
 
 
         return null;
     }
-//        // if size>1 , look for fish first then chicken and then lamb
-//        for (Order order : orderlist.values()) {
-//            if (order.getFoodType().equals("fish") || order.getFoodType().equals("chicken")) {
-//                int orderKey = 0;
-//                System.out.println(order.getFoodType() + " Order Ready!");
-//                for (int key : orderlist.keySet()) {
-//                    if (orderlist.get(key) == order) {
-//                        orderKey = key;
-//                        break;
-//                    }
-//                }
-//                orderlist.remove(orderKey);
-//                return order;
-//            }
-//        }
-//        return null;
-//    }
-
-    //        for (String foodType : new String[]{"fish", "chicken", "lamb"}) {
-//            for (Map.Entry<Integer, Order> entry : orderlist.entrySet()) {
-//                Order order = entry.getValue();
-//                if (order.getFoodType().equals(foodType)) {
-//                    System.out.println(order.getFoodType() + " Order Ready!");
-//                    orderlist.remove(entry.getKey());
-//                }
-//            }
-//        }
-
+    
+    // complexity O(1)
     public static void takeOrder(Order order) {
         orderlist.put(key,order);
         key++;
